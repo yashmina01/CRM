cd039d41d0a3f8586e37276c37270732211024c9
Yash Mina <yashkhodia01@gmail.com>
2023-08-05 17:22:50 +0530

    Adding new user feature added

--- website/urls.py
+++ website/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for crm project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from . import views
 urlpatterns = [
@@ -23,4 +7,5 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('record/<int:pk>', views.customer_records, name='record'),
     path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
+    path('add_record/', views.add_record, name='add_record'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm,AddRecordForm
 from .models import Record
 # Create your views here.
 
@@ -19,11 +19,10 @@ def home(request):
             messages.success(request, "You have successfully logged in ")
             return redirect("home")
         else:
-            messages.success(
-                request, "There was an error to logging you in , try again later")
+            messages.success(request, "There was an error to logging you in , try again later")
             return redirect("home")
     else:
-        return render(request, 'home.html', {'records': records})
+        return render(request, 'home.html', {'records':records})
 
 
 def logout_user(request):
@@ -67,3 +66,16 @@ def delete_record(request, pk):
     else:
         messages.success(request, "You have to login to Delete records")
         return redirect('home')
+
+def add_record(request):
+    form =AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Successfully Added!...")
+                return redirect("home")
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "OOPS! You must be logged in..")
+        return redirect("home")
